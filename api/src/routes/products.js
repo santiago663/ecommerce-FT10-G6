3fd202da8293f32fe16a92dc06b30be3e84e559c
lucia <69209369+Lls28es@users.noreply.github.com>
2021-04-09 17:30:26 -0300

    modif ruta product.js y minimos detalles de org del codigo

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -1,12 +1,63 @@
 const server = require('express').Router();
+const products = require('../utils/products.json');
+
 const { Products } = require('../db.js');
 
+
+
 server.get('/', (req, res, next) => {
+
 	Products.findAll()
 		.then(products => {
 			res.send(products);
 		})
 		.catch(next);
 });
+
+
+server.get('/id/:id', (req, res)=>{
+
+	const id = req.params.id;
+	
+    Products.findByPk(id)
+        .then(resp => {
+           return res.json(resp)
+        }).catch((error) =>{
+            console.error(error.message)
+        })
+});
+
+
+server.post('/', (req, res)=>{
+
+    const { 
+        name, 
+        description, 
+        price, 
+        available, 
+        fileLink, 
+        preview, 
+        authorId,
+        seriesId
+        } = req.body
+
+    Products.findOrCreate({
+
+		    where:{
+                name: name,
+                description: description,
+                price: price,
+                available: available,
+                fileLink: fileLink,
+                preview: preview,
+                authorId: authorId,
+                seriesId: seriesId
+			}
+  
+        }).then(resp => {
+            console.log(resp)
+            res.send('ok')
+        })
+})
 
 module.exports = server;
