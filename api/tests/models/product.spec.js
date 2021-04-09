2ebd67564552896037a0164413f067ae5efe7d59
cristian-hr <cristian.hrauh@gmail.com>
2021-04-08 21:32:50 -0400

    status_1

--- api/tests/models/product.spec.js
+++ api/tests/models/product.spec.js
@@ -1,23 +1,87 @@
-const { Product, conn } = require('../../src/db.js');
+const { Products, conn } = require('../../src/db.js');
 const { expect } = require('chai');
 
-describe('User model', () => {
+describe('Product Model', () => {
   before(() => conn.authenticate()
     .catch((err) => {
       console.error('Unable to connect to the database:', err);
     }));
   describe('Validators', () => {
-    beforeEach(() => Product.sync({ force: true }));
+    beforeEach(() => Products.sync({ force: true }));
     describe('name', () => {
-      it('should throw an error if name is null', (done) => {
-        Product.create({
+      
+      it('should throw an error if name is not a string', (done) => {
+        Products.create({
+          name:1,
+          description:"product1",
+          price: 99.99,
+          available: true,
+          discount: 0.2
         })
-          .then(() => done(new Error('It requires a valid name')))
+          .then(() => done(new Error('Name must be an String')))
           .catch(() => done());
       });
-      it('should work when its a valid name', () => {
-        Product.create({ name: 'Producto' });
+
+      it('should throw an error if description is not a string', (done) => {
+        Products.create({
+          name: "product1",
+          description: 1,
+          price: 99.99,
+          available: true,
+          discount: 0.2
+        })
+          .then(() => done(new Error('Description must be an String')))
+          .catch(() => done());
       });
+
+      it('should throw an error if price is not a decimal', (done) => {
+        Products.create({
+          name: "product1",
+          description: "product1",
+          price: "1",
+          available: true,
+          discount: 0.2
+        })
+          .then(() => done(new Error('Price must be a decimal')))
+          .catch(() => done());
+      });
+
+      it('should throw an error if available is not a boolean', (done) => {
+        Products.create({
+          name: "product1",
+          description: 1,
+          price: 99.99,
+          available: 1,
+          discount: 0.2
+        })
+          .then(() => done(new Error('Available must be a boolean')))
+          .catch(() => done());
+      });
+
+      it('should throw an error if discount is not a float', (done) => {
+        Products.create({
+          name: "product1",
+          description: 1,
+          price: 99.99,
+          available: true,
+          discount: 100
+        })
+          .then(() => done(new Error('Discount must be a float')))
+          .catch(() => done());
+      });
+
+      it('should throw an error if discount is not defined', (done) => {
+        Products.create({
+          name: "product1",
+          description: 1,
+          price: 99.99,
+          available: true
+        })
+          .then(() => done(new Error('Discount cannot be undefined')))
+          .catch(() => done());
+      });      
+
+
     });
   });
 });
