842b4268df4be309cfa331a49743ac0fe8fe76c4
chawi reds <nahudaro@gmails.com>
2021-04-09 09:45:23 -0300

    route detail without test

--- api/src/db.js
+++ api/src/db.js
@@ -25,6 +25,7 @@ fs.readdirSync(path.join(__dirname, '/models'))
 modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
+
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
