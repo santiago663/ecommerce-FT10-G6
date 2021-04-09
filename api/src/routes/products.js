70f947840443f334dedcdbc849b4643b244f339b
Andromata <zar.armandoangel@gmail.com>
2021-04-09 16:12:42 -0300

    Ruta Post de Productos

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -9,4 +9,46 @@ server.get('/', (req, res, next) => {
 		.catch(next);
 });
 
+server.get('/:id', (req, res)=>{
+	
+	Videogames.findByPk(id)
+		.then(resp => {
+		   return res.json(resp)
+		}).catch((error) =>{
+			console.error(error.message)
+		})
+});
+
+server.post('/', (req, res)=>{
+    // const elem = products;
+
+    const { 
+        name, 
+        description, 
+        price, 
+        available, 
+        fileLink, 
+        preview, 
+        authorId,
+        seriesId
+        } = req.body
+
+    Products.findOrCreate({
+            where:{
+                name: name,
+                description: description,
+                price: price,
+                available: available,
+                fileLink: fileLink,
+                preview: preview,
+                authorId: authorId,
+                seriesId: seriesId
+            }
+
+                
+        }).then(resp => {
+            res.send('Producto cargado correctamente')
+        })
+})
+
 module.exports = server;
