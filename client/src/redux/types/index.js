0042a95df3aae8d088eff5d490c3d83e492e3b9f
Enrique <>
2021-04-10 12:40:30 -0300

    feat(client/ component): Added functionality for Product component

--- client/src/redux/types/index.js
+++ client/src/redux/types/index.js
@@ -1,2 +1,3 @@
-export const GET_ALL = 'GET_ALL';
+export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
+export const GET_ONE_PRODUCT = 'GET_ONE_PRODUCT';
 export const FILTER_ARTISTS = 'FILTER_ARTISTS';
