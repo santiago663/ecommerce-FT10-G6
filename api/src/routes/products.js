76905075d8f20bbcd4a07d0fa06890273677f021
chawi reds <nahudaro@gmails.com>
2021-04-10 16:09:35 -0300

    task#S21 refactor(routes/: GET /products/ )

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -1,52 +1,55 @@
 const server = require('express').Router();
 const products = require('../utils/products.json');
 
-const { Products } = require('../db.js');
+const { Products, Categories, Authors } = require('../db.js');
 
 
+server.get('/', async (_req, res, next) => {
+	
+	try {
 
-server.get('/', (req, res, next) => {
 
-	Products.findAll()
-		.then(products => {
-			res.send(products);
-		})
-		.catch(next);
+		let products = await Products.findAll({
+			attributes: ['id', 'name', 'description', 'price', 'available', 'fileLink', 'preview'],
+			include: [{ model: Authors },{ model: Categories }],
+			
+		});
+
+		products === null ? res.send('hubo un error al encontrar los productos') : res.json(products);
+		
+
+	} catch (err) {
+		res.status(401).send(err.message);
+	}
 });
 
 
 
-server.get('/:id', (req, res)=>{
-
+server.get('/:id', (req, res) => {
 	const id = req.params.id;
-	
-    Products.findByPk(id)
-        .then(resp => {
-			if(resp === null){
-				return res.send("Producto inexistente")
+
+	Products.findByPk(id)
+		.then((resp) => {
+			if (resp === null) {
+				return res.send('Producto inexistente');
 			}
-           return res.json(resp)
-        }).catch((error) =>{
-            console.error(error.message)
-        })
+			return res.json(resp);
+		})
+		.catch((error) => {
+			console.error(error.message);
+		});
 });
 
-server.post('/', (req, res)=>{
-	
-	const { 
-	name, 
-	description, 
-	price, 
-	available, 
-	fileLink, 
-	preview, 
-	authorId,
-	seriesId
-	} = req.body
 
-    Products.findOrCreate({
 
-		where:{
+
+
+
+server.post('/', (req, res) => {
+	const { name, description, price, available, fileLink, preview, categories, authorId, seriesId } = req.body;
+	console.log("esto es req body ---->",req.body)
+	Products.findOrCreate({
+		where: {
 			name: name,
 			description: description,
 			price: price,
@@ -54,57 +57,53 @@ server.post('/', (req, res)=>{
 			fileLink: fileLink,
 			preview: preview,
 			authorId: authorId,
-			seriesId: seriesId
-			}       
-	}).then(resp => {
-		res.status(201).json(resp[0])
-	}).catch(err => {
-		res.status(401).send(err.message)
+			seriesId: seriesId,
+		},
 	})
+		.then((newProduct) => {
+			
+			categories.forEach((id) => newProduct[0].addCategories(id));
+			res.json(newProduct[0]);
+		})
+		.catch((err) => {
+			res.status(401).send(err.message);
+		});
 });
 
-server.put('/:id', (req, res)=>{
-	const idProduct = req.params.id
+server.put('/:id', (req, res) => {
+	const idProduct = req.params.id;
 
-	const { 
-		name, 
-		description, 
-		price, 
-		available, 
-		fileLink, 
-		preview, 
-		authorId,
-		seriesId
-	} = req.body
+	const { name, description, price, available, fileLink, preview, authorId, seriesId } = req.body;
 
-	Products.update({
-		name: name,
-		description: description,
-		price: price,
-		available: available,
-		fileLink: fileLink,
-		preview: preview,
-		authorId: authorId,
-		seriesId: seriesId
-	}, {
-
-		where: {
-			id: idProduct,
-		}
-	}).then(() => {
-	
-		Products.findOrCreate({
-
+	Products.update(
+		{
+			name: name,
+			description: description,
+			price: price,
+			available: available,
+			fileLink: fileLink,
+			preview: preview,
+			authorId: authorId,
+			seriesId: seriesId,
+		},
+		{
 			where: {
 				id: idProduct,
-			}
-		}).then(resp2 =>{
-			res.status(201).json(resp2[0])
-		}).catch( () => {
-			res.status(401).send("Producto inexistente")
+			},
+		}
+	).then(() => {
+		Products.findOrCreate({
+			where: {
+				id: idProduct,
+			},
 		})
-	
-	})
+			.then((resp2) => {
+				res.status(201).json(resp2[0]);
+			})
+			.catch(() => {
+				res.status(401).send('Producto inexistente');
+			});
+	});
 });
 
 module.exports = server;
