f3a5ef24bebc77582b7b2ec50c03f56a79da55b4
Lenin Garizabalo <lcamargo@protonmail.com>
2021-04-10 17:20:44 -0500

    task#S29 remove(api/index.js: remove test information on this file).

--- api/index.js
+++ api/index.js
@@ -17,40 +17,14 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
 
-const {Products, Series, Authors} = conn.models
-//PRUEBA INGRESO DE DATOS EN AUTORES SE DEBE BORRAR
-
-const seed = () => {
-  return Promise.all([
-    Authors.findOrCreate({
-      where:{
-        "name": "Sam Gilliam",
-        "email": "",
-      }
-    }),
-  ])
-};
-const seed2 = () => {
-  return Promise.all([
-    Series.findOrCreate({
-      where:{
-        "name": "Sam Gilliam",
-        "description": "alalalalla",
-
-      }
-    }),
-  ])
-};
+const { Products, Series, Authors } = conn.models;
 
 // Syncing all the models at once.
 conn.sync({ truncate: true }).then(() => {
-  
   server.listen(3001, () => {
-    seed();
-    seed2();
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
