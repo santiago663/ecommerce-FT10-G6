c18a6d211ffcf4de38c3b226e6f446a66c1d9816
cristian-hr <cristian.hrauh@gmail.com>
2021-04-10 19:57:33 -0400

    ft(redux/actions): Nueva accion | ft(api/app) Allow Methods

---
+++ client/src/dbArtists.js
@@ -0,0 +1,83 @@
+/* eslint-disable  */
+export default [
+    {
+      "commission": "",
+      "email": "",
+      "idAuthors": 1,
+      "name": "Sam Gilliam",
+      "origin": "United States",
+      "paymentData": "",
+    },
+  
+    {
+      "commission": "",
+      "email": "",
+      "idAuthors": 2,
+      "name": "Mary Lovelace O'Neal",
+      "origin": null,
+      "paymentData": "",
+    },
+  
+    {
+      "commission": "",
+      "email": "",
+      "idAuthors": 3,
+      "name": "Louise Fishman",
+      "origin": "United States",
+      "paymentData": "",
+    },
+  
+    {
+      "commission": "",
+      "email": "",
+      "idAuthors": 4,
+      "name": "Liz Larner",
+      "origin": null,
+      "paymentData": "",
+    },
+  
+    {
+      "commission": "",
+      "email": "",
+      "idAuthors": 5,
+      "name": "Jennifer Pastor",
+      "origin": null,
+      "paymentData": "",
+    },
+  
+    {
+      "commission": "",
+      "email": "",
+      "idAuthors": 6,
+      "name": "Lynda Benglis",
+      "origin": "United States",
+      "paymentData": "",
+    },
+  
+    {
+      "commission": "",
+      "email": "",
+      "idAuthors": 7,
+      "name": "Silke Otto-Knapp",
+      "origin": null,
+      "paymentData": "",
+    },
+  
+    {
+      "commission": "",
+      "email": "",
+      "idAuthors": 8,
+      "name": "Suzanne Jackson",
+      "origin": null,
+      "paymentData": "",
+    },
+  
+    {
+      "commission": "",
+      "email": "",
+      "idAuthors": 9,
+      "name": "Lloyd Morgan Yost",
+      "origin": null,
+      "paymentData": "",
+    }
+  ]
