1d8efdd877ca26e258dfae9abf4349c108f4778f
Lenin Garizabalo <lcamargo@protonmail.com>
2021-04-10 19:02:43 -0500

    update

--- api/seeders/20210410204852-categories-demo.js
+++ api/seeders/20210410204852-categories-demo.js
@@ -3,16 +3,16 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return await queryInterface.bulkInsert("categories", [
-      { name: "Black and White" },
-      { name: "Portrait" },
-      { name: "Color" },
-      { name: "Ilustration" },
-      { name: "Abstract" },
-      { name: "Nature" },
-      { name: "Animals" },
-      { name: "Psikodelic" },
-      { name: "Subrealist" },
-      { name: "Hyper-realist" },
+      { id: 1, name: "Black and White" },
+      { id: 2, name: "Portrait" },
+      { id: 3, name: "Color" },
+      { id: 4, name: "Ilustration" },
+      { id: 5, name: "Abstract" },
+      { id: 6, name: "Nature" },
+      { id: 7, name: "Animals" },
+      { id: 8, name: "Psikodelic" },
+      { id: 9, name: "Subrealist" },
+      { id: 10, name: "Hyper-realist" },
     ]);
   },
 
