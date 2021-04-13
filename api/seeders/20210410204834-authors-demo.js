65072c0465c9763ee08440a821ba20edfee5eb76
Lenin Garizabalo <lcamargo@protonmail.com>
2021-04-13 15:45:50 -0500

    updates seeder author

--- api/seeders/20210410204834-authors-demo.js
+++ api/seeders/20210410204834-authors-demo.js
@@ -53,6 +53,76 @@ module.exports = {
         name: "Cristina Fernandez",
         email: "henry10@ecommerce.com",
       },
+      {
+        id: 11,
+        name: "Felipe San Pedro",
+        email: "henry011@ecommerce.com",
+      },
+      {
+        id: 12,
+        name: "Oscar Carballo Corcino",
+        email: "henry12@ecommerce.com",
+      },
+      {
+        id: 13,
+        name: "Alicia Imas",
+        email: "henry13@ecommerce.com",
+      },
+      {
+        id: 14,
+        name: "Elena Bissinger",
+        email: "henry14@ecommerce.com",
+      },
+      {
+        id: 15,
+        name: "Jesse Montenegro",
+        email: "henry15@ecommerce.com",
+      },
+      {
+        id: 16,
+        name: "Pol Ledent",
+        email: "henry16@ecommerce.com",
+      },
+      {
+        id: 17,
+        name: "Giuseppe Sticchinain",
+        email: "henry17@ecommerce.com",
+      },
+      {
+        id: 18,
+        name: "Ramón Pascual josa",
+        email: "henry18@ecommerce.com",
+      },
+      {
+        id: 19,
+        name: "Henciu Daniel Bulimar",
+        email: "henry19@ecommerce.com",
+      },
+      {
+        id: 20,
+        name: "Leonid Afrémov",
+        email: "henry20@ecommerce.com",
+      },
+      {
+        id: 21,
+        name: "Omar Sorriente",
+        email: "henry21@ecommerce.com",
+      },
+      {
+        id: 22,
+        name: "Clara de Bobes",
+        email: "henry22@ecommerce.com",
+      },
+      {
+        id: 23,
+        name: "Javier Pacheco Almeyda",
+        email: "henry23@ecommerce.com",
+      },
+      {
+        id: 24,
+        name: "Julio Aguilar Montoya",
+        email: "henry24@ecommerce.com",
+      },
     ]);
   },
 
@@ -60,4 +130,3 @@ module.exports = {
     return await queryInterface.bulkDelete("authors", null, {});
   },
 };
-
