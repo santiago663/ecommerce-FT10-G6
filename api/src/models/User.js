a168774c2c2a6e7b773148eb4e2841cb21eee7c4
Lenin Garizabalo <lcamargo@protonmail.com>
2021-04-14 03:16:45 -0500

    Add user model.

---
+++ api/src/models/User.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "users",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone_Number: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      location_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      role_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      available: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
