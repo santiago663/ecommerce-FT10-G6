d748ef512ffd5b84a0a8fabf46101ab21165f7be
cristian-hr <cristian.hrauh@gmail.com>
2021-04-14 17:23:28 -0400

    refactor(api/users post): Return a msg w exist

--- api/src/routes/users/POST.js
+++ api/src/routes/users/POST.js
@@ -13,8 +13,10 @@ server.post("/", async (req, res) => {
         available,
     } = req.body;
 
-    if (!name || !email || !password || !phone_Number || !location_id || !role_id || !available) {
+    if (!name || !email || !password) {
+
         return res.status(422).json({ error: "Data is missing" })
+
     }
 
     try {
@@ -29,10 +31,34 @@ server.post("/", async (req, res) => {
                 phone_Number,
                 location_id,
                 role_id,
-                available,
+                available
             }
         })
-        res.status(200).json(users[0])
+        if(users[1] === false && users[0].available === true) {
+            return res.status(200).json({message: "User already exists"}) 
+        }
+        else if (users[1] === false && users[0].available === false){
+            var guestUser = await Users.update(
+                {
+                    name,
+                    password,
+                    phone_Number,
+                    location_id,
+                    role_id,
+                    available: true
+                },
+                {
+                    where: { email },
+                    returning: true,
+                    plain: true
+                }
+                
+            )
+            return res.status(200).json(guestUser)
+        }
+        
+        res.status(200).json(users)
+
     } catch (error) {
         console.log(error)
         res.status(500).send({ message: "Internal server error" })
