46841b3354d72aefd87d6d5ac313d0c0a63593b6
Andromata <zar.armandoangel@gmail.com>
2021-04-14 15:58:53 -0300

    feat(client/components client/redux):component/action creatión for paginate

--- client/src/components/Catalogue/Catalogue.jsx
+++ client/src/components/Catalogue/Catalogue.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+/*eslint-disable*/
+import React, { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import ProductCard from '../ProductCard/ProductCard';
+import Pagination from '../Pagination/Pagination';
 import Loading from '../Loading/Loading';
 import '../../scss/containers/_catalogue.scss';
 
 function Catalogue() {
   const allProduct = useSelector((store) => store.reducerProduct.allProductCache);
+  const productsPerPage = useSelector((store) => store.reducerPagination.productsPerPage)
+  const currentPage = useSelector((store) => store.reducerPagination.currentPage)
   const loading = useSelector((store) => store.reducerLoading.loading);
-
+ 
+  const indexLastProduct = currentPage * productsPerPage
+  const indexFirstProduct = indexLastProduct - productsPerPage
+  const currentProducts = allProduct.slice(indexFirstProduct, indexLastProduct)
+  
   if (loading) {
     return (
       <div className="loader-container">
@@ -17,13 +25,18 @@ function Catalogue() {
   }
 
   return (
-    <div className="catalogue-wrapper">
-      {
-        allProduct && allProduct.map((artwork) => (
-          <ProductCard key={artwork.id} data={artwork} />
-        ))
-      }
-    </div>
+    <Fragment>
+      <div className="catalogue-wrapper">
+        {
+          currentProducts && currentProducts.map((artwork) => (
+            <ProductCard key={artwork.id} data={artwork} />
+          ))
+        }
+      </div>
+      <div className="paginate">
+      <Pagination cardsPerPage = {productsPerPage} totalCards = {allProduct.length} />
+      </div>
+    </Fragment>
   );
 }
 
