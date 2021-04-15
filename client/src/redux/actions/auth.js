840174cd8843228a628dfcb57f688a11c0141864
Ezequiel Rivas <cheize@hotmail.com>
2021-04-15 09:00:17 -0300

    week02_task#A9_frontend

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -2,9 +2,14 @@
 import * as TYPES from '../types/index';
 
 export const login = (uid, displayName) => ({
-    type: TYPES.login,
+    type: TYPES.AUTH_LOGIN,
     payload: {
         uid,
         displayName
+    }
+})
+export const logout = (uid, displayName) => ({
+    type: TYPES.AUTH_LOGOUT,
+    payload: {
     }
 })
