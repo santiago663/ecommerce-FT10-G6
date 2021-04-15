b594a9328537ee204774ac39fbb3ad4944d604b1
cristian-hr <cristian.hrauh@gmail.com>
2021-04-15 17:58:40 -0400

    feat(api/users delete): Empty user cart

---
+++ api/src/routes/orders/DELETE.js
@@ -0,0 +1,35 @@
+const server = require("express").Router();
+const { Orders, Products } = require("../../db");
+
+server.delete("/users/:idUser/cart", async (req, res) => {
+
+    const { idUser } = req.params
+
+    try {
+        let cart = await Orders.findOne({
+            where: {
+                userId: idUser,
+                state: "open"
+            },
+
+            include: [{
+                model: Products,
+                through: { attributes: [] },
+            }],
+        })
+
+        if (!cart) return res.status(411).json({ message: "User doesn't exist" })
+
+        let array = cart.products.map(prod => prod.id)
+
+        cart.removeProducts(array)
+
+        res.status(200).json({ message: "Cart is empty" })
+
+    } catch (err) {
+        console.log(err)
+        res.status(401).send({ message: "Internal server error" });
+    }
+});
+
+module.exports = server;
