792a45d0aea1e15fc75c1bee99ff90d75ff9b8cd
lucia <69209369+Lls28es@users.noreply.github.com>
2021-04-16 17:00:12 -0300

    agregando scss y reducerError

---
+++ client/src/redux/reducers/reducerErrorRoutes.js
@@ -0,0 +1,61 @@
+/*eslint-disable*/
+import * as TYPES from '../types/index';
+
+const initialState = {
+
+    postProduct: [],
+    postProductError: [],
+    putProduct: [],
+    putProductError: [],
+    deleteProduct: [],
+    deleteProductError: [],
+};
+
+export default function reducerErrorRoutes(state = initialState, action) {
+ 
+    switch (action.type) {
+
+        case TYPES.POST_NEW_PRODUCT:
+            return {
+                ...state,
+                postProduct: action.payload,
+            };
+
+        case TYPES.POST_NEW_PRODUCT_ERROR:
+            return {
+                ...state,
+                postProductError: action.payload,
+            };
+
+        case TYPES.PUT_EDIT_PRODUCT_BYID:
+        case TYPES.PUT_EDIT_PRODUCT_CATEGORY:
+            return {
+                ...state,
+                putProductError: action.payload,
+            };
+
+        case TYPES.PUT_EDIT_PRODUCT_BYID_ERROR:
+        case TYPES.PUT_EDIT_PRODUCT_CATEGORY_ERROR:
+            return {
+                ...state,
+                putProductError: action.payload,
+            };
+
+        case TYPES.DELETE_PRODUCT_CATEGORY:
+        case TYPES.DELETE_ONE_PRODUCT:
+            return {
+                ...state,
+                deleteProduct: action.payload,
+            };
+
+        case TYPES.DELETE_ONE_PRODUCT_ERROR:
+        case TYPES.DELETE_ONE_PRODUCT_ERROR:
+            return {
+                ...state,
+                deleteProductError: action.payload,
+            };
+
+        default:
+            return state;
+    }
+}
