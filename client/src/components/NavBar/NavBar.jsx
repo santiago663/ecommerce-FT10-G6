3ef4b3bbf9e6b94c7df9959f7e60730650c0e9e4
Cristian Hernández <74212972+cristian-hr@users.noreply.github.com>
2021-04-17 02:35:26 -0400

    Update NavBar.jsx

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable  */
-import React, {} from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import '../../scss/components/_navBar.scss';
