54816d78aa55cbc6d888ddc53603da3189b97ae7
lucia <69209369+Lls28es@users.noreply.github.com>
2021-04-18 01:39:22 -0300

    refactor client/src/redux

---
+++ client/src/redux/reducers/reducerOrderUser.js
@@ -0,0 +1,30 @@
+/*eslint-disable*/
+
+import * as TYPES from '../types/index';
+
+const initialState = {
+
+  allOrders: [],
+  allUsers: [],
+};
+
+export default function reducerOrderUser (state = initialState, action){
+
+  switch (action.type) {
+
+    case TYPES.GET_ALL_ORDERS:
+      return {
+        ...state,
+        allOrders: action.payload,
+      };
+
+    case TYPES.GET_ALL_USERS:
+    return {
+        ...state,
+        allUsers: action.payload,
+    };
+  
+    default:
+      return state;
+  }
+};
