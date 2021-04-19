44366d719db9961041a5fe9bb83a52ba51c06cfa
Lenin Garizabalo <lcamargo@protonmail.com>
2021-04-18 20:39:07 -0500

    reafactor user guest

--- client/src/redux/reducers/reducerShoppingCart.js
+++ client/src/redux/reducers/reducerShoppingCart.js
@@ -1,24 +1,31 @@
 /*eslint-disable*/
 
-import * as TYPES from '../types/index';
+import * as TYPES from "../types/index";
 
 const initialState = {
-	shoppingCart: [],
+  shoppingCart: [],
 };
 
 export default function reducerShoppingCart(state = initialState, action) {
-	switch (action.type) {
-		case TYPES.ADD_TO_CART:
-			return {
-				...state,
-				shoppingCart: [...state.shoppingCart].concat(action.payload),
-			};
-		case TYPES.REMOVE_FROM_CART:
-			return {
-				...state,
-				shoppingCart: state.shoppingCart.filter((item) => item.id !== action.payload.id),
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case TYPES.LOAD_STORAGE_GUEST:
+      return {
+        ...state,
+        shoppingCart: action.payload,
+      };
+    case TYPES.ADD_TO_CART:
+      return {
+        ...state,
+        shoppingCart: [...state.shoppingCart].concat(action.payload),
+      };
+    case TYPES.REMOVE_FROM_CART:
+      return {
+        ...state,
+        shoppingCart: state.shoppingCart.filter(
+          (item) => item.id !== action.payload.id
+        ),
+      };
+    default:
+      return state;
+  }
 }
