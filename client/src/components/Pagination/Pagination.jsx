d93c4e8c7bb902daea666d1db6a000aedc02adb3
cristian-hr <cristian.hrauh@gmail.com>
2021-04-19 21:06:04 -0400

    feat(client/catalog/pagi): Next/Prev button

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,6 +1,6 @@
 /*eslint-disable*/
 import React from 'react';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { paginate } from '../../redux/actions/request';
 import '../../scss/components/_pagination.scss';
 
@@ -12,16 +12,28 @@ function Pagination({ cardsPerPage, totalCards }) {
     pageNumbers.push(i);
   }
 
+  const page = useSelector((store) => store.reducerPagination.currentPage);
+
+  function previusPage(page) {
+    if (page > 1) dispatch(paginate(page - 1))
+  }
+
+  function nextPage(page, finalPage) {
+    if (page < finalPage) dispatch(paginate(page + 1))
+  }
+
   return (
     <div>
       <ul className="pagination">
+      <button className="pageButton" onClick={() => previusPage(page)}>Previus</button>
         {
           pageNumbers.map((number) => (
-            <li key={number} className="page-item'">
+            <li key={number} className={ page == number ? "page-item-select" : "page-item'"}>
               <a onClick={() => dispatch(paginate(number))} href="#!" className="page-link">{number}</a>
             </li>
           ))
         }
+        <button className="pageButton" onClick={() => nextPage(page, Math.ceil(totalCards / cardsPerPage))}>Next</button>
       </ul>
     </div>
   );
