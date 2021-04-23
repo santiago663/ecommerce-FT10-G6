39633a65e472b2295d7559be1e967c5232b3ff16
lucia <69209369+Lls28es@users.noreply.github.com>
2021-04-23 18:44:27 -0300

    sendgrid 100emai/day-2-BACK

--- api/src/routes/auth/POST.js
+++ api/src/routes/auth/POST.js
@@ -1,10 +1,12 @@
 const server = require("express").Router();
-const sgMail = require('@sendgrid/mail')
+// const sgMail = require('@sendgrid/mail');
 const { Users, Orders, Products } = require("../../db");
 
-// sgMail.setApiKey("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");
-
-sgMail.setApiKey("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");
+// import { sengridEmail } from '../../services/sengridEmail.js'
+// import {stateOrder} from '../../services/stateOrder.js'
+const stateOrder = require ('../../services/stateOrder.js');
+const discountProduct = require ('../../services/discountProduct');
+const sengridEmail = require ('../../services/sengridEmail.js');
 
 server.post("/signup", async(req, res) => {
 
@@ -25,84 +27,41 @@ server.post("/signup", async(req, res) => {
                 state: state
             }, include: [Products]
         })
-        if(orders.products.length === 0){
-            res.status(422).json({message:"the order has no products"});
+        if(orders === null || !orders.products){
+            
+            res.status(422).json({message:`the user does not have an order in ${state} status`});
         }
         else{
-            
-            let orderID = orders.id;
-            let total = orders.total;
-            let date = orders.date;
-            let state = orders.state;
-    
-            var i = 0
-            while(orders.products.length > i ){
-                
-                if(orders.products[i]){
+            var dateBasicOrder = []
 
-                    product= {
-                        product:orders.products[i].name,
-                        image: orders.products[i].preview,
-                        price:orders.products[i].price,
-                    }
-                    dataOrder.push(product)
-                }
-                i++
-            }
-            const msg = {
-                
-                to: "lls28programacion@hotmail.com",
-                from: "lu_23-7-92@hotmail.com",
-                subject: "Testing Node Emailulitma11?",
-                html: `
-              
-                <div>
-                    <h2>${name}: DigitalArt te informa que tu orden ID:${orderID}, con fecha de creación ${date} se encuentra en estado ${state}</h2>
-                        <table width="300" height="50" align="center" >
-                            <tr>
-                                <h2>Products:</h2>
-                    ${dataOrder.map((m) => {
-                        return(`
-                            <div>
-                                <table width="300" height="50" align="center" style="background:#FFFFFF; padding:15px 15px 15px 15px; border:solid 2px #E5E5E5; border-bottom:5; width:48%">
-                                    <tr bgcolor="#6a7da5" >
-                                        <td >
-                                            <img src="${m.image}" alt="${m.product} style="border:solid 1px #E5E5E5; border-radius:5" height="125" align="center" />
-                                        </td>
-                                        <th align="center" style="padding: 0 20px 20px 0;"height="115"  style="padding: 0 20px 20px 0;">
-                                            producto:${m.product}, precio: $${m.price}
-                                        </th>
-                                    </tr>
-                                    
-                                </table>
-                            </div>
-                            `
-                        )
-                    })}
-                                <h2>Valor Total de la compra: $${total}</h2>
-                            </tr>
-                        </table>
+            let orderID = orders.id;
+            let date = orders.date;
+            let total = orders.total;        
+            let state = orders.state;
+
+            dateBasicOrder.push(orderID, date, state, total)
+
+            if(dateBasicOrder.length === 4){
+                var i = 0
+                while(orders.products.length > i ){
                     
-                    <h2>Gracias por tu Compra</h2>
-                </div>
-                
-                `
+                    if(orders.products[i]){
+
+                        product = {
+                            product:orders.products[i].name,
+                            image: orders.products[i].preview,
+                            price:orders.products[i].price,
+                        }
+                        dataOrder.push(product)
+                    }
+                    i++
+                }
             }
-            sgMail.send(msg, function(err, info){
-               if(err){
-                   console.log("Email Not Send",err.response.body.errors)
-               } 
-               else{
-                   console.log("Email Send",info)
-               }
-            })
-         
-            // var message = await transporter.sendMail({
-            //     to: user.email,
-            //     from:"no-reply@onsta.com",
-            //     subject: "signup success",
-            //     html:`<h1>algo${orderID},${total}</h1>`
-            // })
+          
+            let msgBody = (stateOrder(name, dateBasicOrder, dataOrder))
+            console.log(msgBody)
+
+            //console.log(sengridEmail(msgBody)) //SENDGRID MANDAR EMAIL, NO BORRAR, SOLO 100 EMAILS POR DIA
 
             return res.status(200).json({message:"email enviado exitosamente"}) 
         }
@@ -112,5 +71,85 @@ server.post("/signup", async(req, res) => {
         res.status(500).json({ message: "Internal server error", status: 500})
     }
 });
+
+
+server.post("/productOffers", async(req, res) => {
+
+    const { name, orderId, email, state } = req.body;
+
+    let dataOrder = []
+    let product ={}
+    try{
+        if(!email || !name || !orderId && state === "open") return res.status(422).json({message:"please add all the fields"});
+        
+        var user = await Users.findOne({
+            where:{email:email}
+        })
+        var orders = await Orders.findOne({
+            where:{
+                userId: user.id,
+                id: orderId,
+                state: state
+            }, include: [Products]
+        })
+        if(orders === null || !orders.products){
+            
+            res.status(422).json({message:`the user does not have an order in ${state} status`});
+        }
+        else{
+
+            var dateBasicOrder = []
+
+            let orderID = orders.id;
+            let date = orders.date;
+            let total = orders.total;        
+            let state = orders.state;
+
+            dateBasicOrder.push(orderID, date, state, total)
+
+            var diez100=(10/100);
+            var TotalDiscount = 0;
+            var i = 0
+            while(orders.products.length > i ){
+                
+                if(orders.products[i]){
+
+                    var productUser = await Products.findOne({
+                        where:{ id:orders.products[i].id}
+                    })
+
+                    if(productUser.price === orders.products[i].price){
+                        var result = productUser.price  *diez100;
+                        var descuento = productUser.price - result;
+                        TotalDiscount = TotalDiscount +descuento
+                        console.log("VALORES___",TotalDiscount, descuento)
+
+                        product = {
+
+                            product:orders.products[i].name,
+                            image: orders.products[i].preview,
+                            price:orders.products[i].price,
+                            priceDiscount: descuento,
+                        }
+                    }
+                    dateBasicOrder.push(TotalDiscount) 
+                    dataOrder.push(product)
+                }
+                i++
+            }
+            
+            let msgBody = (discountProduct(name, dateBasicOrder, dataOrder))
+
+            //console.log(sengridEmail(msgBody)) //SENDGRID MANDAR EMAIL, NO BORRAR, SOLO 100 EMAILS POR DIA
+
+            return res.status(200).json({message:"email enviado exitosamente"}) 
+        }
+          
+    }catch(error) {
+        console.log(error)
+        res.status(500).json({ message: "Internal server error", status: 500})
+    }
+});
+
 
 module.exports = server;
