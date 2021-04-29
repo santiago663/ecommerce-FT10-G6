99fb976db43ed46b50c535972c28faa0b8af1b7d
chawi reds <nahudaro@gmails.com>
2021-04-28 22:29:18 -0300

    azza y score

--- client/src/components/Filters/Azza.jsx
+++ client/src/components/Filters/Azza.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './_filter.scss';
 import { orderAsc, orderByCategories, orderByAuthor, orderStar, getBackup } from '../../redux/actions/actionFront';
-import Score from './Score'
+import Score from './Score';
 
 function Filter() {
 	const [toggle, setToggle] = useState(false);
@@ -12,12 +12,11 @@ function Filter() {
 	const allArtist = useSelector((state) => state.reducerArtist.allArtistCache);
 	const selectAuthor = useSelector((state) => state.reducerProduct.author);
 	const selectCategorie = useSelector((state) => state.reducerProduct.categorie);
-	const selectScore = useSelector((state) => state.reducerProduct.score);
 	const disponibleAuthor = useSelector((state) => state.reducerProduct.authorDisponible);
 	const disponibleCategories = useSelector((state) => state.reducerProduct.contegorieDisponible);
 
 	const handleFilter = (e) => {
-		dispatch(orderByCategories(e.target.value));
+		e.target.value === 'All' ? dispatch(getBackup()) : dispatch(orderByCategories(e.target.value));
 	};
 
 	const handleOrder = (e) => {
@@ -25,9 +24,8 @@ function Filter() {
 		toggle ? setToggle(false) : setToggle(true);
 	};
 
-
 	const handleAuthor = (e) => {
-		dispatch(orderByAuthor(e.target.value));
+		e.target.value === 'All' ? dispatch(getBackup()) : dispatch(orderByAuthor(e.target.value));
 	};
 	const handleBackUp = (e) => {
 		e.preventDefault();
@@ -35,84 +33,54 @@ function Filter() {
 	};
 
 	return (
-		<>			
-				<div className="Container-Filter">
-					<button className="Boton-Reset" onClick={(e) => handleBackUp(e)}>
-						{`Reset`}
-					</button>
-					<div className="filter">
-						{(selectAuthor && selectScore) ||
-							(selectScore && selectCategorie) ||
-							(selectCategorie && selectAuthor) ? (
-							<button onClick={(e) => handleBackUp(e)} className="Button-Try-Again">
-								Try Again
-							</button>
-						) : selectAuthor || selectCategorie ? (
-							<>
-								<select onChange={(e) => handleFilter(e)}>
-									<option default>Filter By Category</option>
-									{selectAuthor
-										? disponibleCategories &&
-										disponibleCategories.map((C) => {
+		<>
+			<div className="Container-Filter">
+				<button className="Boton-Reset" onClick={(e) => handleBackUp(e)}>
+					{`Reset`}
+				</button>
+				<div className="filter">
+					{
+						<>
+							<select onChange={(e) => handleFilter(e)}>
+								<option default value="All">
+									Filter By Category
+								</option>
+								{selectAuthor
+									? disponibleCategories &&
+									  disponibleCategories.map((C) => {
 											return <option value={C}>{C}</option>;
-										})
-										: allCategories &&
-										allCategories.map((C) => {
+									  })
+									: allCategories &&
+									  allCategories.map((C) => {
 											return <option value={C.name}>{C.name}</option>;
-										})}
-								</select>
+									  })}
+							</select>
 
-								<select onChange={(e) => handleAuthor(e)}>
-									<option default>Filter By Author</option>
-									{!selectCategorie
-										? allArtist &&
-										allArtist.map((a) => {
+							<select onChange={(e) => handleAuthor(e)}>
+								<option default value="All">
+									Filter By Author
+								</option>
+								{!selectCategorie
+									? allArtist &&
+									  allArtist.map((a) => {
 											return <option value={a.name}>{a.name}</option>;
-										})
-										: disponibleAuthor &&
-										disponibleAuthor.map((a) => {
+									  })
+									: disponibleAuthor &&
+									  disponibleAuthor.map((a) => {
 											return <option value={a}>{a}</option>;
-										})}
-								</select>
-							</>
-						) : (
-							<>
-								<select onChange={(e) => handleFilter(e)}>
-									<option default>Category</option>
-									{selectAuthor
-										? disponibleCategories &&
-										disponibleCategories.map((C) => {
-											return <option value={C}>{C}</option>;
-										})
-										: allCategories &&
-										allCategories.map((C) => {
-											return <option value={C.name}>{C.name}</option>;
-										})}
-								</select>
+									  })}
+							</select>
+							<Score />
+						</>
+					}
 
-								<select onChange={(e) => handleAuthor(e)}>
-									<option default>Author</option>
-									{!selectCategorie
-										? allArtist &&
-										allArtist.map((a) => {
-											return <option value={a.name}>{a.name}</option>;
-										})
-										: disponibleAuthor &&
-										disponibleAuthor.map((a) => {
-											return <option value={a}>{a}</option>;
-										})}
-								</select>
-								<Score />
-							</>
-						)}
-
-						{!toggle ? (
-							<button className="fas fa-sort-alpha-down" value="asc_name" onClick={(e) => handleOrder(e)} />
-						) : (
-							<button className="fas fa-sort-alpha-up" value="desc_name" onClick={(e) => handleOrder(e)} />
-						)}
-					</div>
-				</div>			
+					{!toggle ? (
+						<button className="fas fa-sort-alpha-down" value="asc_name" onClick={(e) => handleOrder(e)} />
+					) : (
+						<button className="fas fa-sort-alpha-up" value="desc_name" onClick={(e) => handleOrder(e)} />
+					)}
+				</div>
+			</div>
 		</>
 	);
 }
--- client/src/components/Filters/Score.jsx
+++ client/src/components/Filters/Score.jsx
@@ -1,59 +1,56 @@
 /*eslint-disable*/
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { orderStar, getBackup, choiseStar } from '../../redux/actions/actionFront';
+import { paginate } from '../../redux/actions/request';
 import './_filter.scss';
 
 function Score() {
 	const [toggle, setToggle] = useState(false);
 	const dispatch = useDispatch();
-	const selectAuthor = useSelector((state) => state.reducerProduct.allProductCache);
-
-
+	const actualStars = useSelector((state) => state.reducerProduct.actualStars);
+	const selectAuthor = useSelector((state) => state.reducerProduct.author);
+	const selectCategorie = useSelector((state) => state.reducerProduct.categorie);
 
 	const handleStar = (e) => {
-		toggle ? setToggle(false) : setToggle(true);
-		dispatch(orderStar(e.target.name));
+		dispatch(orderStar(e.target.value));
 
+		toggle ? setToggle(false) : setToggle(true);
 	};
 
 	const handleChoiseStar = (e) => {
-		e === 'All' ? dispatch(getBackup()) : dispatch(choiseStar(e));
-
-
-	}
+		e === 'All' ? dispatch(getBackup()) : (dispatch(paginate(1)), dispatch(choiseStar(e)));
+	};
 
 	return (
 		<>
-
-
 			<select onChange={(e) => handleChoiseStar(e.target.value)}>
 				<option value="All">Stars</option>
-				<option value="1">1</option>
-				<option vlaue="2">2</option>
-				<option value="3">3</option>
-				<option vlaue="4">4</option>
-				<option value="5">5</option>
+				{selectAuthor || selectCategorie ? (
+					actualStars.map((s) => {
+						return <option value={s}>{s}</option>;
+					})
+				) : (
+					<>
+						<option value="0">0</option>
+						<option value="1">1</option>
+						<option vlaue="2">2</option>
+						<option value="3">3</option>
+						<option vlaue="4">4</option>
+						<option value="5">5</option>
+					</>
+				)}
 			</select>
 
 			<div className="Container-Iconos">
-				{toggle ? (
-					<button
-						name="starUp"
-						className="far fa-star "
-						id="star"
-						onClick={(e) => handleStar(e)}
-					></button>
+				{selectAuthor || selectCategorie ? (
+					''
+				) : toggle ? (
+					<button value="starUp" className="far fa-star " id="star" onClick={(e) => handleStar(e)}></button>
 				) : (
-					<button
-						name="starDown"
-						className="fas fa-star"
-						id="star"
-						onClick={(e) => handleStar(e)}
-					></button>
+					<button value="starDown" className="fas fa-star" id="star" onClick={(e) => handleStar(e)}></button>
 				)}
 			</div>
-
 		</>
 	);
 }
