e3b21b81766ab5f83702838a9dacec1c9e62a3e5
Ezequiel Rivas <cheize@hotmail.com>
2021-05-02 13:30:27 -0300

    cambioss

--- client/src/components/User/Components/Wishlist.jsx
+++ client/src/components/User/Components/Wishlist.jsx
@@ -13,7 +13,7 @@ export default function Wishlist() {
 
     const dispatch = useDispatch();
 
-    const wishlistUser = useSelector((store) => store.reducerWishlist.wishlist); 
+    const wishlistUser = useSelector((store) => store.reducerWishlist.wishlist);
 
     const openPreview = (e, image) => {
         e.preventDefault()
@@ -23,62 +23,61 @@ export default function Wishlist() {
 
     const deleteProduct = e => {
 
-        let body ={
+        let body = {
             userId: wishlistUser.userId,
             productId: e,
         }
-        
-        if(body.userId && body.productId){
-            dispatch( deleteUserWhislist( body) )
+
+        if (body.userId && body.productId) {
+            dispatch(deleteUserWhislist(body))
         }
     }
 
     return (
         <>
-        <div className="profile-body-wishlist ">
-            <div className="title">
-                <h1>My Wishlist</h1>
-            </div>
-            <hr className="divisor" />
-            <div className="tableheader tableheader-wishlist">
-                <div className="libraryPreviewTitle-wishlist previewTitle-wishlist"><h4>Preview</h4></div>
-                <div className="libraryPreviewTitle-wishlist nameTitle-wishlist"><h4>Name</h4></div>
-                <div className="libraryPreviewTitle-wishlist price-wishlist"><h4>Price</h4></div>
-                <div className="libraryPreviewTitle-wishlist toBuy-wishlist"><h4>To Buy / Detail</h4></div>                 
-            </div>
-            <div className="myProductsResults">
-                {wishlistUser?.length !== 0 &&
-                    wishlistUser?.products.map((product) => (
-                        <>
-                            <div className="libraryPreview-wishlist" >
-                                <div className="divImage-wishlist">
-                                    <img className="myProductsImage" src={product.preview} alt="" onClick={(e) => openPreview(e, product.preview)}/>                                    
-                                </div>                               
-                                <div className="libraryProductName-wishlist">
-                                    <h4> {product.name}</h4>
+            <div className="profile-body-wishlist ">
+                <div className="title">
+                    <h1>My Wishlist</h1>
+                </div>
+                <hr className="divisor" />
+                <div className="tableheader tableheader-wishlist">
+                    <div className="libraryPreviewTitle-wishlist previewTitle-wishlist"><h4>Preview</h4></div>
+                    <div className="libraryPreviewTitle-wishlist nameTitle-wishlist"><h4>Name</h4></div>
+                    <div className="libraryPreviewTitle-wishlist price-wishlist"><h4>Price</h4></div>
+                    <div className="libraryPreviewTitle-wishlist toBuy-wishlist"><h4>To Buy / Detail</h4></div>
+                </div>
+                <div className="myProductsResults">
+                    {wishlistUser?.length !== 0 && wishlistUser.products?.length !== 0 &&
+                        wishlistUser.products.map((product) => (
+                            <>
+                                <div className="libraryPreview-wishlist" >
+                                    <div className="divImage-wishlist">
+                                        <img className="myProductsImage" src={product.preview} alt="" onClick={(e) => openPreview(e, product.preview)} />
+                                    </div>
+                                    <div className="libraryProductName-wishlist">
+                                        <h4> {product.name}</h4>
+                                    </div>
+                                    <div className="libraryProductPrice-wishlist">
+                                        <h4> ${product.price}</h4>
+                                    </div>
+                                    <div className=" libraryDownload-wishlist">
+                                        <button
+                                            className=" button-wishlist"
+                                            type="button"
+                                            key={product.id}
+                                            onClick={() => deleteProduct(product.id)}
+                                        >
+                                            <GiIcons.GiHeartMinus />
+                                        </button>
+                                    </div>
                                 </div>
-                                <div className="libraryProductPrice-wishlist">
-                                    <h4> ${product.price}</h4>
-                                </div>
-                                <div className=" libraryDownload-wishlist">
-                                    <button
-                                        className=" button-wishlist"
-                                        type="button"
-                                        key={product.id}
-                                        onClick={()=>deleteProduct(product.id)}
-                                    >
-                                        <GiIcons.GiHeartMinus />
-                                    </button> 
-                                   
-                                    <Link to={`/product/${product.id}`}>
-                                        <AiIcons.AiFillEye />
-                                    </Link>
-                                </div>
-                            </div>                            
-                        </>
-                    ))}
+                                <Link to={`/product/${product.id}`}>
+                                    <AiIcons.AiFillEye />
+                                </Link>
+                            </>
+                        ))}
+                </div>
             </div>
-        </div>
         </>
     )
 }
