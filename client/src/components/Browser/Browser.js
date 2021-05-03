28e9c06993be37e323b586ccd37c4376cc796f2a
cristian-hr <cristian.hrauh@gmail.com>
2021-05-03 12:26:29 -0400

    refactor(client/filter) Filter bar height

--- client/src/components/Browser/Browser.js
+++ client/src/components/Browser/Browser.js
@@ -1,14 +1,12 @@
 /*eslint-disable*/
 import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
-import SearchBar from '../SearchBar/SearchBar';
 import Products from '../Products/Products';
 
 
 function Browser() {
 	return (
 		<div className="bigest-container">
-			<SearchBar />
 			<Route path="/Browser/Products" component={Products} />
 		</div>
 	);
