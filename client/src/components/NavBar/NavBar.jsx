bf123a08c1bdcc5a3f41a6b7ca3ed4138d848965
Lenin Garizabalo <lcamargo@protonmail.com>
2021-05-02 21:17:17 -0500

    hidden about link on navBar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -29,68 +29,86 @@ function NavBar() {
   useEffect(() => { }, [currentUser]);
 
   return (
-		<nav className="navbar">
-			<div className="navleft">
-				<Link to="#" className="sandwich">
-					{menu === '0' ? (
-						<FaIcons.FaBars onClick={() => showSidebar('1')} className="iconmenu" />
-					) : (
-						<HiIcons.HiChevronDoubleLeft onClick={() => showSidebar('0')} className="iconmenu" />
-					)}
-				</Link>
-
-				<ul className="nav-buttons_navigation">
-					<Link className="nav-link" to="/">
-						<li>Home</li>
-					</Link>
-					<Link className="nav-link" to="/Browser/products">
-						<li>Find Art</li>
-					</Link>
-					<Link to="/about">
-						<li>About Us</li>
-					</Link>
-				</ul>
-			</div>
-			<div className="nav-buttons_authentication">
-				{currentUser?.profilePic ? (
-					<div className="navbarProfilePic">
-						<Link to="/user/profile">
-							<img src={currentUser.profilePic} type="file" alt="profilePic" />
-						</Link>
-					</div>
-				) : null}
-				{(currentUser[0] || currentUser['id']) && (
-					<Link to="/user/wishlist">
-						<button className="signin--btn btn-secondary wishlist">
-							<FaHeart />
-						</button>
-					</Link>
-				)}
-				<ul className="Icon-Cart">
-					<Link to="/checkout">
-						<i className="fas fa-shopping-cart ">{shoppingCart.length > 0 && shoppingCart.length}</i>
-					</Link>
-				</ul>
-				{currentUser?.name ? (
-					<button type="button" className="signin--btn logout" onClick={handleLogOut}>
-						Log out
-					</button>
-				) : (
-					<>
-						<Link to="/signin">
-							<button className="signin--btn btn-primary signNav" type="button">
-								Sign in
-							</button>
-						</Link>
-						<Link to="/register">
-							<button className="signin--btn btn-secondary signNav" type="button">
-								Sign up
-							</button>
-						</Link>
-					</>
-				)}
-			</div>
-		</nav>
+    <nav className="navbar">
+      <div className="navleft">
+        <Link to="#" className="sandwich">
+          {menu === "0" ? (
+            <FaIcons.FaBars
+              onClick={() => showSidebar("1")}
+              className="iconmenu"
+            />
+          ) : (
+            <HiIcons.HiChevronDoubleLeft
+              onClick={() => showSidebar("0")}
+              className="iconmenu"
+            />
+          )}
+        </Link>
+        <ul className="nav-buttons_navigation">
+          <Link className="nav-link" to="/">
+            <li>Home</li>
+          </Link>
+          <Link className="nav-link" to="/Browser/products">
+            <li>Find Art</li>
+          </Link>
+          {location.pathname !== "/about" ? (
+            <Link to="/about">
+              <li>About Us</li>
+            </Link>
+          ) : (
+            ""
+          )}
+        </ul>
+      </div>
+      <div className="nav-buttons_authentication">
+        {currentUser?.profilePic ? (
+          <div className="navbarProfilePic">
+            <Link to="/user/profile">
+              <img src={currentUser.profilePic} type="file" alt="profilePic" />
+            </Link>
+          </div>
+        ) : null}
+        {(currentUser[0] || currentUser["id"]) && (
+          <Link to="/user/wishlist">
+            <button className="signin--btn btn-secondary wishlist">
+              <FaHeart />
+            </button>
+          </Link>
+        )}
+        <ul className="Icon-Cart">
+          <Link to="/checkout">
+            <i className="fas fa-shopping-cart ">
+              {shoppingCart.length > 0 && shoppingCart.length}
+            </i>
+          </Link>
+        </ul>
+        {currentUser?.name ? (
+          <button
+            type="button"
+            className="signin--btn logout"
+            onClick={handleLogOut}
+          >
+            Log out
+          </button>
+        ) : (
+          <>
+            <Link to="/signin">
+              <button className="signin--btn btn-primary signNav" type="button">
+                Sign in
+              </button>
+            </Link>
+            <Link to="/register">
+              <button
+                className="signin--btn btn-secondary signNav"
+                type="button"
+              >
+                Sign up
+              </button>
+            </Link>
+          </>
+        )}
+      </div>
+    </nav>
   );
 }
 
