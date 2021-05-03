d447c262e093310861a5a5a0a9a0909a625a4670
Lenin Garizabalo <lcamargo@protonmail.com>
2021-05-02 20:18:06 -0500

    remove paticles component

--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -1,14 +1,11 @@
 /* eslint-disable  */
 import "../../scss/components/_about.scss";
 import Card from "./Card";
-import Particles from "react-particles-js";
-import { particles } from "./style/particles";
 import team from "./team";
 
 export default function About() {
   return (
     <div className="home-wrapper">
-      {/* <Particles id="particles-js" params={particles} /> */}
       <div className="section-about">
         <div className="team">
           {team.information.map((member, index) => {
