6e843ce641d48dee61d640f3849a67a6ff6b1704
chawi reds <nahudaro@gmails.com>
2021-05-04 02:20:14 -0300

    carousel primer intento

--- client/src/components/Carousel/ImagCarousel.js
+++ client/src/components/Carousel/ImagCarousel.js
@@ -7,63 +7,17 @@ import { addToCartUser, removeToCartUser } from '../../redux/actions/actionOrder
 
 import './_slider.scss';
 
-function imagCarousel({ src, id, products,props }) {
-	
-	const currentUser = useSelector((store) => store.auth.currentUser);
-	const dispatch = useDispatch();
-	const currentOrder = useSelector((store) => store.reducerOrderUser.currentOrder);
-	const shoppingCart = useSelector((state) => state.reducerShoppingCart.shoppingCart);
-
-	const handleAddToCart = (productOnClick) => {
-		if (currentUser.id) {
-			dispatch(addToCartUser(productOnClick));
-		} else {
-			dispatch(addToCart(productOnClick));
-		}
-	};
-
-	const handleRemoveFromCart = (productOnClick) => {
-		if (currentUser.id) {
-			dispatch(removeToCartUser(productOnClick));
-		} else {
-			dispatch(removeFromCart(productOnClick));
-		}
-	};
-
-	let lStorage;
-	if (shoppingCart.length !== 0) {
-		if (shoppingCart.filter((prod) => prod.id === id).length === 1) {
-			lStorage = true;
-		}
-	}
-
-	
-
+function imagCarousel({ src, id, products, props }) {
 	return (
 		<>
 			{src === undefined ? (
 				<img src={src} className="imgStyles" />
 			) : (
 				<>
-					<h1 className="title-image">Novedad!</h1>
+					<h1 className="title-image">New Works !!</h1>
 					<Link to={`/product/${id}`}>
-						<img src={src} className={props.props ? "imgStylesInCheckout" : "imgStyles"} />
+						<img src={src} className={props.props ? 'imgStylesInCheckout' : 'imgStyles'} />
 					</Link>
-
-					{!lStorage ? (
-						<i className="fas fa-cart-plus add" key={id} onClick={() => handleAddToCart(products)}></i>
-					) : (
-						<i
-							className="fas fa-cart-arrow-down remove"
-							key={id}
-							onClick={() => handleRemoveFromCart(products)}
-						>
-							<br />
-						</i>
-					)}
-					<div className="description-slider">
-						<p>{products?.name} </p>
-					</div>
 				</>
 			)}
 		</>
--- client/src/components/Carousel/Slider.js
+++ client/src/components/Carousel/Slider.js
@@ -1,6 +1,6 @@
 /* eslint-disable  */
-import React,{useState,useEffect} from 'react';
-import ImagCarousel from './ImagCarousel'
+import React, { useState, useEffect } from 'react';
+import ImagCarousel from './ImagCarousel';
 import { useSelector } from 'react-redux';
 import { FaAngleDoubleLeft, FaAngleDoubleRight } from 'react-icons/fa';
 import Loading from '../Loading/Loading';
@@ -8,59 +8,63 @@ import './_slider.scss';
 
 function Slider(props) {
 	const loading = useSelector((store) => store.reducerLoading.loading);
-	const products = useSelector((state) => state.reducerProduct.allProductCache)
-
-	let japanese = products.filter((x) => 'Japanese Art' !== x.name);
-	let fondo = "https://cdn.discordapp.com/attachments/829710868413546511/829924062276485121/face.jpg"
+	const products = useSelector((state) => state.reducerProduct.allProductCache);
+	let fondo = 'https://cdn.discordapp.com/attachments/829710868413546511/829924062276485121/face.jpg';
 	let idProduct = [];
 	let srcPreview = [];
 	let productObject = [];
-	products.forEach(x=>{
-		if ('Japanese Art' === x.name){
-			srcPreview.push(x);
-			
-		} 
+	products.forEach((x) => {
+		srcPreview.push(x.preview);
+		idProduct.push(x.id);
+	});
+ let sliderArr;
+	props.props
+		? (sliderArr = [
+				<ImagCarousel />,
+				<ImagCarousel src={srcPreview[26]} id={idProduct[26]} products={products[26]} props={props} />,
+				<ImagCarousel />,
+				<ImagCarousel src={srcPreview[29]} id={idProduct[29]} products={products[29]} props={props} />,
+				<ImagCarousel />,
+				<ImagCarousel src={srcPreview[5]} id={idProduct[5]} products={products[5]} props={props} />,
+				<ImagCarousel />,
+				<ImagCarousel src={srcPreview[25]} id={idProduct[25]} products={products[25]} props={props} />,
+				<ImagCarousel />,
+				<ImagCarousel src={srcPreview[98]} id={idProduct[98]} products={products[98]} props={props} />,
+				<ImagCarousel />,
+				<ImagCarousel src={srcPreview[36]} id={idProduct[36]} products={products[36]} props={props} />,
+		  ])
+		: (sliderArr = [
+				
+				<ImagCarousel src={srcPreview[0]} id={idProduct[0]} products={products[0]} props={props} />,
+				<ImagCarousel src={srcPreview[1]} id={idProduct[1]} products={products[1]} props={props} />,
+				<ImagCarousel />,
+				<ImagCarousel src={srcPreview[2]} id={idProduct[2]} products={products[2]} props={props} />,
+				<ImagCarousel src={srcPreview[3]} id={idProduct[3]} products={products[3]} props={props} />,
+				<ImagCarousel />,
+				<ImagCarousel src={srcPreview[4]} id={idProduct[4]} products={products[4]} props={props} />,
+		  ]);
+
+	const [autoPlay, setAutoPlay] = useState(true);
+	const [x, setX] = useState(0);
+	let interval;
+
+	
+
+	useEffect(()=>{
+
+	return()=> clearInterval(interval)
+	},[])
+props.props ? (interval = setInterval(() => {goRight();}, 5000)): null;
+
+	const goRight = () => {
+		clearInterval(interval);
+		x === 0 ? setX(-100 * (sliderArr.length - 1)) : setX(x + 100);
+	};
+
+	const goLeft = () => {
 		
-		;})
-	console.log('esto se muestra en la linea 18 :', srcPreview);
-	let sliderArr = [
-		<ImagCarousel />,
-		<ImagCarousel src={srcPreview[0].preview} id={srcPreview[0].id} products={products[0]} props={props} />,
-		<ImagCarousel />,
-		// <ImagCarousel src={srcPreview} id={idProduct[1]} products={products[1]} props={props} />,
-		// <ImagCarousel />,
-		// <ImagCarousel src={srcPreview} id={idProduct[2]} products={products[2]} props={props} />,
-		// <ImagCarousel />,
-		// <ImagCarousel src={srcPreview[3]} id={idProduct[3]} products={products[3]} props={props} />,
-		// <ImagCarousel />,
-		// <ImagCarousel src={srcPreview[4]} id={idProduct[4]} products={products[4]} props={props} />,
-	];
-	
-	
-	const[autoPlay,setAutoPlay] = useState(true)
-	const [x,setX] = useState(0)
-	
-	let interval= setInterval(() => {
-					goLeft()
-			}, 5000)
-	// const functionSlider = () =>{
-	// 	setAutoPlay(false)
-	// 	autoPlay
-	// 		? (interval = setInterval(() => {
-	// 				goRight();
-	// 		}, 5000))
-	// 		: clearInterval(interval);
-	// }
-	
-	
-	const goRight = () =>{clearInterval(interval);
-	x === 0 ? setX(-100 * (sliderArr.length - 1)) : setX(x + 100);};
-
-	const goLeft = () =>{clearInterval(interval);
-	 x === -100*(sliderArr.length-1) ? setX(0) : setX(x - 100)};
-	
-
-	
+		x === -100 * (sliderArr.length - 1) ? setX(0) : setX(x - 100);
+	};
 
 	if (loading) {
 		return (
@@ -70,7 +74,7 @@ function Slider(props) {
 		);
 	}
 	return (
-		<div className={props.props? 'sliderHome' : 'slider'}>
+		<div className={props.props ? 'sliderHome' : 'slider'}>
 			{sliderArr.map((item, index) => {
 				return (
 					<div
@@ -83,12 +87,18 @@ function Slider(props) {
 				);
 			})}
 			<div>
-				<button id="toLeft" onClick={goLeft} >
-					<FaAngleDoubleLeft className="icono" />
-				</button>
-				<button id="toRight" onClick={goRight}>
-					<FaAngleDoubleRight className="icono" />
-				</button>
+				{props.props ? (
+					<div></div>
+				) : (
+					<>
+						<button id="toLeft" onClick={goLeft}>
+							<FaAngleDoubleLeft className="icono" />
+						</button>
+						<button id="toRight" onClick={goRight}>
+							<FaAngleDoubleRight className="icono" />
+						</button>
+					</>
+				)}
 			</div>
 		</div>
 	);
