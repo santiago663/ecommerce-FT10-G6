a5b40b599ed64c3453d385c5560cf80518ac4a22
chawi reds <nahudaro@gmails.com>
2021-05-05 23:26:31 -0300

    action y reducers

--- client/src/redux/actions/actionBack.js
+++ client/src/redux/actions/actionBack.js
@@ -591,3 +591,26 @@ export const editProductStock = (stock) => {
     }
   }
 };
+
+export const sendDiscountToBack = (objectWithDiscount) =>{
+  (dispatch) => {
+		try {
+			dispatch(requestData());
+			axios
+				.get(`http://localhost:3001/get/review?productId=${objectWithDiscount}`)
+				.then((res) => {
+					dispatch({
+						type: TYPES.UPLOAD_PRODUCTS_WITH_DISCOUNT,
+						payload: res.data,
+					});
+					dispatch(requestSuccess());
+				})
+				.catch((error) => console.error(error));
+		} catch (error) {
+			dispatch({
+				type: TYPES.GET_ALL_USERS_ERROR,
+				payload: error,
+			});
+		}
+  };
+} 
